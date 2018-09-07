c7e8167e8b9c1464bfc860e97ff99a4912ea6ee0
root <ermuoz1014@gmail.com>
2018-09-06 23:14:28 -0500

    tftp funcionando

--- tftp/cliente.c
+++ tftp/cliente.c
@@ -1,4 +1,4 @@
-#include <sys/socket.h>
+	#include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h> /* superset of previous */
 #include <stdlib.h>
@@ -14,22 +14,20 @@ int operacion(unsigned char*trama){
 	opt_code <<= 8;
 	opt_code += trama[1];	
 	return opt_code;
+
 }
 
-int secuenciaACK(unsigned char* trama){
-	unsigned char secuencia = trama[2];
+int secuenciaDATA(unsigned char* trama){
+	short secuencia = trama[2];
 	secuencia <<=8;
-	secuencia +=trama[3];
+	secuencia +=trama[3] ;
 	return secuencia;
 }
-
-unsigned char datosACK(unsigned char* trama){
-	unsigned char *origen;
-	for(int  i = 0; i<sizeof(trama)-4; i++){
-		printf("%hu ", trama[i+4]);
-		origen = trama[i+4];
+void datosDATA(unsigned char* trama, int tam, unsigned char* dest){
+	for(int  i = 0; i<tam; i++){
+		dest[i] = trama[i+4];
 	}
-	return origen;
+
 }
 int peticionLectura(unsigned char* trama, char * nombreArchivo){
 	trama[0] = 0;
@@ -41,24 +39,32 @@ int peticionLectura(unsigned char* trama, char * nombreArchivo){
 }
 
 //WRQ
-void peticionEscritura(unsigned char* trama, char* nombreArchivo){
+int  peticionEscritura(unsigned char* trama, char* nombreArchivo){
 	trama[0] = 0;
 	trama[1] = 2;
 	strcpy(trama+2, nombreArchivo);
+	strcpy(trama+3+strlen(nombreArchivo),"octet");
+	trama[strlen(nombreArchivo) + strlen("octet") + 3] = 0;
+	return strlen(nombreArchivo)+strlen("octet") +4;
 	
 }
 //ACK
 void formatoACK(unsigned char* trama, int bloque){
 	trama[0] = 0;
 	trama[1] = 4;
-	trama[3] = (bloque & 0xff00) >> 8;
-	trama[4] = (bloque & 0x00ff);
+	trama[2] = (bloque & 0xff00) >> 8;
+	trama[3] = (bloque & 0x00ff);
 }
 
-void formatoDATA(unsigned char* trama, int indice, FILE *archivo){
+void formatoDATA(unsigned char* trama, int indice, unsigned char* datos, int tam){
 	trama[0] = 0;
 	trama[1] = 3;
-	strcpy(trama+2,archivo);
+	trama[2] = (indice  >> 8) & 0x00ff;
+	trama[3] = indice & 0x00ff;
+
+	for (int i= 0; i<tam;i++){
+		trama[i+4] = datos[i];
+	}
 }
 int tamArchivo(FILE *arch){
 	fseek(arch, 0, SEEK_END); 
@@ -66,14 +72,7 @@ int tamArchivo(FILE *arch){
 	fseek(arch, 0, SEEK_SET); 
 	return tam;
 }
-void escribirArchivo(FILE *fp, int offset){
-	fp=fopen("c:\\test.txt", "w");
-	if(fp==NULL){
-		printf("Error abriendo el archivo \n");
-	}else{
-		fprintf(fp, "Testing...\n");
-	}
-}
+
 
 
 
@@ -110,53 +109,64 @@ int main(int argc, char *argv[]){
 			memset(&remota,0x00,sizeof(remota));
 			remota.sin_family = AF_INET;
 			remota.sin_port = htons(69);
+			size_t tamSock = sizeof(remota);
 			printf("ip %s", argv[1]);
 			inet_pton(AF_INET, argv[1], &(remota.sin_addr));
 			remota.sin_addr.s_addr = inet_addr(argv[1]);
 			unsigned char * mensaje [1024];
 			char nombre[20];
 			//char *buffer[10000];
-			int opc;
-			printf("\n\n+++++++ Menu TFTP ++++++\n 1 --> peticion de lectura \n 2 --> peticion de lectura \n ");
-			scanf("%d",&opc);
-			switch(opc){
+			int accion = 1;
+			while(accion){
+			printf("\n\n+++++++ Menu TFTP ++++++\n 1 --> peticion de lectura \n 2 --> peticion de escritura \n ");
+			scanf("%d",&accion);
+			switch(accion){
 				case 1:
+						fflush();
 						printf("Escribe el nombre del archivo \n");
 						scanf("%s",nombre);
 						int tam = peticionLectura(mensaje, nombre);
 						if(sendto(udp_socket,mensaje,tam,0,(struct sockaddr*)&remota,sizeof(remota)) != -1){
-							int aux = recvfrom(udp_socket, mensaje, 516, 0, (struct sockaddr*)&remota, &remota);
+							int aux = recvfrom(udp_socket, mensaje, 516, 0, (struct sockaddr*)&remota, &tamSock);
 							if( aux == -1){
 								printf("Error al recibir \n");
 							}else{
 								printf("Exito al recibir \n");
 								if(operacion(mensaje)==3){
-									int sec = secuenciaACK(mensaje);
+									int sum,t;
 									archivo = fopen(nombre,"wb+");
-									printf("tam %d", aux);
 									if(archivo!=NULL){
-										while(aux-4>=512){
-											printf("numero de secuencia %d",sec);
-											fwrite(mensaje+4,1,512,archivo);
-											printf("Tamanio bloque %d\n", aux - 4);
-											formatoACK(mensaje,sec);
-											if(sendto(udp_socket, mensaje, aux, 0, (struct sockaddr*)&remota, sizeof(remota))){
-												printf("Error al enviar ACK %hu\n", sec);
-											}else{
-												printf("Exito al enviar ACK %hu\n", sec);
-											}
-
-											tam = recvfrom(udp_socket, mensaje, 516, 0, (struct sockaddr*)&remota, &remota); 
-											//seguir con el flujo del tftp		
-											}
-											printf("jamas entro prro \n");
+										while(aux-4<=512){
+											t= aux -4;
+											sum+=t;
+											unsigned char datos[t];
+											datosDATA(mensaje, t,datos);
+											fwrite(datos,1,t,archivo);
+											formatoACK(mensaje,secuenciaDATA(mensaje));
 											
+											if(sendto(udp_socket, mensaje, aux, 0, (struct sockaddr*)&remota, sizeof(remota))==-1){
+												printf("Error al enviar ACK %hu\n", secuenciaDATA(mensaje));
+											}
+											if(aux-4<512){
+												
+												printf("numero de paquetes %d \n",secuenciaDATA(mensaje));
+												printf("bytes leidos %d",sum);
+												break;
+											}
+											aux = recvfrom(udp_socket, mensaje, 516, 0, (struct sockaddr*)&remota, &tamSock); 
+											//seguir con el flujo del tftp	
+											
+										
+											
+											}
+											
+											fclose(archivo);
 										}
 										else{
 											printf("Error en el archivo");
 										}
 								}else{
-									printf("Error no se encuentra el archivo ");
+									printf("Error no se encuentró el archivo %d",operacion(mensaje));
 								}
 							}
 							
@@ -170,19 +180,44 @@ int main(int argc, char *argv[]){
 						scanf("%s",nombre);
 						archivo = fopen(nombre,"rb");
 						if(archivo!=NULL){
-							int t = tamArchivo(archivo);
-							char* buffer = (char*)malloc(t);
-							fread(buffer,tamArchivo(archivo),1,archivo);
-							printf("tamaño de archivo %d",t);
-							int veces = t/512;
-							int i = 0;
-							while(i==veces){
-
+							int tam = peticionEscritura(mensaje,nombre);
+							if(sendto(udp_socket, mensaje,tam,0,(struct sockaddr*)&remota, sizeof(remota))!=-1){
+								int t = recvfrom(udp_socket,mensaje,516,0,(struct sockaddr*)&remota, &tamSock);
+								if(t==-1){
+									printf("Error con la conexión al servidor d\n ");
+								}else{
+									if(operacion(mensaje)==4){
+										int datos[512];
+										int bytes= fread(datos,1,512,archivo);
+										int veces=1;
+										int tam=0;
+										printf("numeros %d",bytes);
+										while(bytes>0){	
+											formatoDATA(mensaje,veces,datos,bytes);	
+											if(sendto(udp_socket,mensaje,bytes+4,0,(struct sockaddr*)&remota, tamSock)==-1){
+												printf("Error al enviar DATA\n");
+											}
+											tam = recvfrom(udp_socket,mensaje,516,0,(struct sockaddr*)&remota,&tamSock);
+											if(tam==-1){
+												
+												printf("error al recibir ACK \n");
+											}
+											bytes = fread(datos,1,512,archivo);
+											veces++;
+										}
+										printf("Se envio el archivo con %d numero de secuencias \n",veces);
+									}else{
+										printf("ERROR!! opc %d",operacion(mensaje));
+									}
+								}
+									
+							}else{
+								printf("Error en la conexión con el servidor");
 							}
+							
 						}
 						else{
-
-							printf("no se pudo abrir el archiv especificado");
+							printf("El archivo no existe!!");
 							return 0;
 						}
 						break;
@@ -192,6 +227,7 @@ int main(int argc, char *argv[]){
 				default: 
 						printf("no haz puesto una operacion valida");
 						break;
+			}
 			}
 
 		}
